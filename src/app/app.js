feabb81445a3dd513e17c2a9cc2c63a4beaaabde
Matthew Yong <yongf@student.unimelb.edu.au>
2019-05-18 19:15:16 +1000

    Added Basic API call

--- src/app/app.js
+++ src/app/app.js
@@ -5,9 +5,22 @@ const app = express();
 app.get('/api/customers', (req,res) => {
     const customers = [
         {
-            
+            id: 1,
+            firstname: 'Matthew',
+            lastname: 'Doe',
+        },
+        {
+            id: 2,
+            firstname: 'John',
+            lastname: 'Doe',
+        },
+        {
+            id: 3,
+            firstname: 'Carolyn',
+            lastname: 'Plant',
         }
     ]
+    res.json(customers);
 })
 
 const port = 3000;
