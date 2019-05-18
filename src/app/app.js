30b21b91a7edec8339f43211c43f4ab4dea63f0d
Matthew Yong <yongf@student.unimelb.edu.au>
2019-05-19 01:09:32 +1000

    Able to print contents of user view

--- src/app/app.js
+++ src/app/app.js
@@ -1,10 +1,25 @@
+//Node Packages
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 const cors = require('cors');
+const nano = require('nano')('http://SinsOnTwitter:group68@localhost:5984');
 
+//Express Variables
 const app = express();
 const port = 5000;
 
+//CouchDB variables
+const users = nano.db.use('site_users');
+
+// Middleware 
+
+//Cors configuration
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+//Routes
 
 app.get('/api/customers', (req,res) => {
     res.contentType('application/json');
@@ -14,5 +29,22 @@ app.get('/api/customers', (req,res) => {
         {id: 3, firstname: "Carolyn", lastname: "Plant"}
     ]);
 })
+
+app.get('/api/users', (req,res) => {
+    res.contentType('application/json');
+    users.view('getUsers', 'get_User_Info',{include_docs: true}).then((body) => {
+        body.rows.forEach((doc) => {
+          //res.send(doc.value);
+          console.log(doc.value);
+        });
+        res.send(body);
+      });
+    /*
+    nano.db.get('site_users').then((body) => {
+        res.send(body);
+    });
+    */
+})
+
 
 app.listen(port, () => console.log(`Server Started on Port ${port}`));
