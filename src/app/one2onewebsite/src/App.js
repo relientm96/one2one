5f4d2af283221c49fe41b9c45c27bede908401dd
relientm96 <matthewyfy@gmail.com>
2019-07-18 00:09:28 +1000

    Added node-modules in gitignore

--- src/app/one2onewebsite/src/App.js
+++ src/app/one2onewebsite/src/App.js
@@ -3,13 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 import Customers from './components/customers/customers'
-import Dashboard from './components/dashboard/dashboard';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Dashboard/>
+        <Customers/>
       </header>
     </div>
   );
